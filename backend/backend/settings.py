e8b868a9c0a1b7e1e1e8ab98316da70214bb45e8
Karina, B. <karina551869@gmail.com>
2026-06-17 00:07:28 +0500

    fix static + templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'core.middleware.SensitiveDataMaskingMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -46,7 +47,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "static"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', REDIS_URL)
@@ -137,7 +138,7 @@ CELERY_TASK_ALWAYS_EAGER = os.getenv('CELERY_TASK_ALWAYS_EAGER', 'False') == 'Tr
 JWT_EXP_SECONDS = int(os.getenv('JWT_EXP_SECONDS', '3600'))
 GOOGLE_CLIENT_ID = os.getenv("836321365787-1rh72ts264c7md67ompc0uc3gfbu5fqj.apps.googleusercontent.com")
 GOOGLE_CLIENT_SECRET = os.getenv("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GOOGLE_CLIENT_SECRET")
-GOOGLE_OAUTH_REDIRECT_URI = os.getenv('GOOGLE_OAUTH_REDIRECT_URI', 'https://pathway-98.onrender.com/api/oauth/google/callback/')
+GOOGLE_OAUTH_REDIRECT_URI = os.getenv('GOOGLE_OAUTH_REDIRECT_URI', 'https://pathway-00.onrender.com/api/oauth/google/callback/')
 CAPTCHA_SECRET_KEY = os.getenv('CAPTCHA_SECRET_KEY', '')
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
@@ -218,3 +219,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
